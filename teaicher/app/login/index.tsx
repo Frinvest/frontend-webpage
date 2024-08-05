4197a97679bcb2f13b423b6f5d854072c7126193
Thang Le <131735016+Thangle8501340@users.noreply.github.com>
2024-08-04 20:02:21 -0700

    Routed from normal page to login page

--- teaicher/app/login/index.tsx
+++ teaicher/app/login/index.tsx
@@ -1,54 +1,16 @@
 import { View,  StyleSheet, Image, Dimensions,TouchableOpacity, Linking } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button} from '@rneui/themed';
-import {Text} from '../components/Text/Text'
-import { Link } from 'expo-router';
+import {Text} from '../../components/Text/Text'
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* Logo */}
-      <View style={styles.hero}>
-        <Image
-          source={require('../assets/images/logo.png')}
-          style={styles.heroImage}
-          resizeMode="contain"
-        />
-      </View>
       {/* Information */}
       <View style={styles.content}>
         <View style={styles.header}>
           <Text style={styles.titleText} font='generic' size='large' color="#FFFFFF">Welcome to TeAIcher</Text>
           <Text style={styles.messageText} font='generic' size='medium' color="#FFFFFF">A personalized and fun way to learn</Text>
         </View>
-        {/* Sign up Button */}
-        <View style={styles.buttonView}>
-          <Button
-              title="SIGN UP"
-              buttonStyle={styles.button}
-              containerStyle={{
-                width: 200,
-                marginHorizontal: 50,
-                marginVertical: 0,
-              }}
-              titleStyle={{ fontWeight: 'bold' }}
-            />
-        </View>
-        {/* Log in  */}
-        <View style={styles.bottom}>
-            <Text style = {styles.bottomText}  font='generic' size='medium'  color="#FFFFFF" >Already got an account?  
-              <View style={styles.login}>
-  
-                  <Link  href="/login"> 
-                      <Text style={styles.loginText} font='generic' size='medium'  color="#FFFFFF" >
-                      Log In 
-                      </Text>
-                  </Link>
-   
-              </View>
-            </Text>
-    
-        </View>
-
       </View>
 
     </SafeAreaView>
